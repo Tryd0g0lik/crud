6b6200ac4861a9f97a6c5889adf08d6aaafe0477
Tryd0g0lik <work80@mail.ru>
2024-01-28 15:55:19 +0700

    [master]: THe 'Button.tsx' has been added

---
+++ src/frontend/src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import React, { JSX } from "react";
+import "./Button.css";
+import { Str } from "../intarfaces.ts";
+
+export default function ButtonFC({ classname, name }: Str): JSX.Element {
+  return (
+    <div className={classname}>
+      <button type="submit">{name}</button>
+    </div>
+  )
+}

e3954cd3ef28ff6b8c09bb043813859cc4281e5b
Tryd0g0lik <work80@mail.ru>
2024-01-31 13:01:08 +0700

    [01]: the function delete was created for boxes

--- src/frontend/src/components/Button/index.tsx
+++ src/frontend/src/components/Button/index.tsx
@@ -1,8 +1,25 @@
-import React, { JSX } from "react";
+import React, { JSX, useEffect } from "react";
 import "./Button.css";
 import { Str } from "../intarfaces.ts";
 
 export default function ButtonFC({ classname, name }: Str): JSX.Element {
+  useEffect(() => {
+    const handler = (e: MouseEvent): void => {
+      const divTarget = (e.target as HTMLDivElement);
+      const divParent = (divTarget.parentElement as HTMLDivElement).parentElement as HTMLDivElement;
+      if (divParent.className.includes("box")) {
+        divParent.remove();
+      };
+    };
+    const divUnmounting = document.querySelector(".unmounting"); // удаляем контейнер
+    if ((divUnmounting !== null) || (divUnmounting !== undefined)) {
+      (divUnmounting as HTMLDivElement).addEventListener("click", handler);
+    };
+
+    return (() => {
+      (divUnmounting as HTMLDivElement).removeEventListener("click", handler);
+    });
+  });
   return (
     <div className={classname}>
       <button type="submit">{name}</button>

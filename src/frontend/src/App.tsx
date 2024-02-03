a7f6d42f151fcf2dceb6cbc0f16b129dd0477670
Tryd0g0lik <work80@mail.ru>
2024-02-03 09:15:20 +0700

    [02]: the code is corrected

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -4,33 +4,31 @@ import HeadFC from "./components/Header/index.tsx";
 import ButtonFC from "./components/Button/index.tsx";
 import BoxiesFC from "./components/Box/index.tsx";
 import TextFC from "./components/Textares/index.tsx";
-import { Name } from "./components/intarfaces.ts";
+import { Name, Str } from "./components/intarfaces.ts";
 let oldData: string = "";
 
 export default function AppFC(): JSX.Element {
   const [arr, setArr] = useState([]);
   const handlerTextarea = (datas: Name) => {
-    const arrJson = JSON.parse(datas as string);
+    const arrJson = JSON.parse(datas);
     const dataArr = arrJson.data;
-    const newArr = (dataArr as any[]).slice();
+    const newArr = (dataArr).slice();
     setArr(newArr);
   };
 
   useEffect(() => {
     const hadlerGetLStorage = () => {
       const lStorage = localStorage.getItem("data");
-      // const divContent = document.querySelector(".content");
+
       if ((lStorage === null) || (lStorage === undefined) ||
         (oldData.includes(lStorage))) {
         return setTimeout(() => {
           hadlerGetLStorage();
         }, 1500);
       }
-      console.log("[LS]: ", typeof lStorage);
+
       const datas = lStorage.slice(0);
-      // const content = (divContent as HTMLDivElement).innerHTML;
       handlerTextarea(datas);
-      // (divContent as HTMLDivElement).innerHTML = content + { arr };
     };
     const divBittonSend = document.querySelector(".textarea .send");
     if (divBittonSend === null || divBittonSend === undefined) {
@@ -40,34 +38,30 @@ export default function AppFC(): JSX.Element {
       hadlerGetLStorage();
     });
     return () => {
-      (divBittonSend as HTMLDivElement).removeEventListener("mousedown", () => {
-        hadlerGetLStorage();
-      });
+      // (divBittonSend as HTMLDivElement).removeEventListener("mousedown", () => {
+      //   hadlerGetLStorage();
+      // });
     };
-  });
+  }, []);
   return (
     <>
       <HeadFC name={"Notes"} classname={"h"} classnameCall={"update-button"} nameCall={""} /* заголовок с кнопкой для обновления */
         Call={(cnameCall: string, nameCall: string) => <ButtonFC classname={cnameCall} name={nameCall} />} />
       <div className="content">
-        {/* < BoxiesFC name="сюда вставляем текст" classname="box" classnameCall="unmounting" /* контейнер с кнопкой для закрытия  */}
-        {/* Call={(cnameCall: string, nameCall: string) => <ButtonFC classname={cnameCall} name={nameCall} /> */}
-        {/* } /> */}
-        {arr.map((data) => (
-
-          < div key={Object.entries(data)[0][0]} >
-            < BoxiesFC name={(Object.entries(data)[0][1] as Record<string, string>).textarea} classname="box" >
-              <ButtonFC classname="unmounting" name="" />
+        {
+          arr.map((data: any) => (
+            < BoxiesFC ind={Object.entries(data)[0][0] as string} name={(Object.entries(data)[0][1] as Record<string, string>).textarea} classname="box" >
+              <ButtonFC ind={Object.entries(data)[0][0] as string} classname="unmounting" name="" />
             </BoxiesFC>
-          </div>
-        ))}
-
+          )
+          )
+        }
       </div>
       <div className="h3">
         <h3>New note</h3>
       </div>
       <TextFC>
-        <ButtonFC classname={"send"} name={""} />
+        <ButtonFC classname="send" name={""} />
       </TextFC>
     </>
   );
